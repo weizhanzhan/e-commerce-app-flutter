4b8262b8f2ab4b22e0e5012dff326c1974a58420
zhanzhan.wei <375786117@qq.com>
2020-04-23 22:08:30 +0800

    tt

--- lib/pages/category/index.dart
+++ lib/pages/category/index.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-class HomePAge extends StatefulWidget {
-  HomePAge({Key key}) : super(key: key);
+class CategoryPage extends StatefulWidget {
+  CategoryPage({Key key}) : super(key: key);
 
   @override
-  _HomePAgeState createState() => _HomePAgeState();
+  _CategoryPageState createState() => _CategoryPageState();
 }
 
-class _HomePAgeState extends State<HomePAge> {
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Text('homepage'),
+       child: Text('CategoryPage'),
     );
   }
 }
